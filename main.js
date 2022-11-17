79ecb9bb300224e957bc5cceabb064b80c4ddf1e
mikerol23 <michaelrolincik@gmail.com>
2022-11-17 12:23:05 -0500

    Run and display calculation when "=" is clicked

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-const add = function (x, y) {
-  return x + y;
-};
-
-const subtract = function (x, y) {
-  return x - y;
-};
-
-const multiply = function (x, y) {
-  return x * y;
-};
-
-const divide = function (x, y) {
-  return x / y;
-};
-
-function operate(x, y, operator) {
-  return operator(x, y);
-}
-
-
-
 const numBtns = document.querySelectorAll(".numbers");
 const operatorBtns = document.querySelectorAll(".operators");
 const equalsBtn = document.querySelector(".equals");
@@ -27,6 +5,33 @@ const equalsBtn = document.querySelector(".equals");
 let activeNumber = document.getElementById("active-number");
 let savedNumber = document.querySelector("#saved-number");
 let operatorContainer = document.querySelector("#operator-container");
+let chosenOperator;
+
+function compute() {
+  let x = parseInt(savedNumber.textContent);
+  let y = parseInt(activeNumber.textContent);
+  let operator = chosenOperator;
+
+  savedNumber.textContent = "";
+  operatorContainer.textContent = "";
+
+  switch (operator) {
+    case "add":
+      activeNumber.textContent = x + y;
+      break;
+    case "subtract":
+      activeNumber.textContent = x - y;
+      break;
+    case "multiply":
+      activeNumber.textContent = x * y;
+      break;
+    case "divide":
+      activeNumber.textContent = x / y;
+      break;
+    default:
+      activeNumber.textContent = "Error";
+  }
+}
 
 numBtns.forEach((button) => {
   button.addEventListener("click", () => {
@@ -34,15 +39,13 @@ numBtns.forEach((button) => {
   });
 });
 
-let firstNumber;
-
-const operatorBtns = document.querySelectorAll(".operators");
-
 operatorBtns.forEach((button) => {
   button.addEventListener("click", () => {
     savedNumber.textContent = activeNumber.textContent;
     activeNumber.textContent = "";
     operatorContainer.textContent = button.textContent;
-    operatorContainer.value = button.id;
+    chosenOperator = button.id;
   });
 });
+
+equalsBtn.addEventListener("click", compute);
